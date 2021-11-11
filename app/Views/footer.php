93f70d0fed0f7fc2e7d06e99e143c72d9c6104ea
atulaive <Atul.aliveinc@gmail.com>
2021-11-11 21:32:58 +0530

    For developement use

---
+++ app/Views/footer.php
@@ -0,0 +1,166 @@
+
+		<!--Footer Section-->
+		<section>
+			<footer class="bg-dark-purple text-white">
+				<div class="footer-main">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-2 col-md-12">
+								<h6>Resources</h6>
+								 <hr class="deep-purple text-primary accent-2 mb-4 mt-0 d-inline-block mx-auto">
+								<ul class="list-unstyled mb-0">
+									<li><a href="javascript:;">Our Team</a></li>
+									<li><a href="javascript:;">Contact US</a></li>
+									<li><a href="javascript:;">About</a></li>
+									<li><a href="javascript:;">Harlequini Dawn</a></li>
+									<li><a href="javascript:;">Blog</a></li>
+									<li><a href="javascript:;">Terms and Conditions</a></li>
+								</ul>
+							</div>
+							<div class="col-lg-3 col-md-12">
+								<h6>Contact</h6>
+								<hr class="deep-purple  text-primary accent-2 mb-4 mt-0 d-inline-block mx-auto">
+								<ul class="list-unstyled mb-0">
+									<li>
+										<a href="#"><i class="fa fa-car mr-3 text-primary"></i> New York, NY 10012, US</a>
+									</li>
+									<li>
+										<a href="#"><i class="fa fa-envelope mr-3 text-primary"></i> info12323@example.com</a></li>
+									<li>
+										<a href="#"><i class="fa fa-phone mr-3 text-primary"></i> + 01 234 567 88</a>
+									</li>
+									 <li>
+										<a href="#"><i class="fa fa-print mr-3 text-primary"></i> + 01 234 567 89</a>
+									</li>
+								</ul>
+							</div>
+							<div class="col-lg-4 col-md-12">
+								<h6>Download App</h6>
+								<hr class="deep-purple  accent-2 mb-4 mt-0 d-inline-block mx-auto">
+								<div class="">
+									<a class="btn bg-store fs-16" href=""><i class="fa fa-apple mr-2"></i> App Store</a>
+									<a class="btn bg-store fs-16" href=""><i class="fa fa-android mr-2"></i> Google Pay</a>
+								</div>
+								<h6 class="mb-0 mt-5">Payments</h6>
+								<hr class="deep-purple  text-primary accent-2 mb-2 mt-3 d-inline-block mx-auto">
+								<div class="clearfix"></div>
+								<ul class="footer-payments">
+									<li class="pl-0"><a href="javascript:;"><i class="fa fa-cc-amex" aria-hidden="true"></i></a></li>
+									<li><a href="javascript:;"><i class="fa fa-cc-visa" aria-hidden="true"></i></a></li>
+									<li><a href="javascript:;"><i class="fa fa-credit-card-alt" aria-hidden="true"></i></a></li>
+									<li><a href="javascript:;"><i class="fa fa-cc-mastercard" aria-hidden="true"></i></a></li>
+									<li><a href="javascript:;"><i class="fa fa-cc-paypal" aria-hidden="true"></i></a></li>
+								</ul>
+							</div>
+							<div class="col-lg-3 col-md-12">
+								<h6>Subscribe</h6>
+								<hr class="deep-purple  text-primary accent-2 mb-4 mt-0 d-inline-block mx-auto">
+								<div class="clearfix"></div>
+								<div class="input-group w-100">
+									<input type="text" class="form-control br-tl-3  br-bl-3 " placeholder="Email">
+									<div class="input-group-append ">
+										<button type="button" class="btn btn-primary br-tr-3  br-br-3"> Subscribe </button>
+									</div>
+								</div>
+								<h6 class="mt-5 mb-3">Follow Us</h6>
+								<hr class="deep-purple  accent-2 mb-4 mt-0 d-inline-block mx-auto">
+								<ul class="list-unstyled list-inline">
+									<li class="list-inline-item">
+										<a class="btn-floating btn-sm rgba-white-slight mx-1 waves-effect waves-light">
+											<i class="fa fa-facebook bg-facebook"></i>
+										</a>
+									</li>
+									<li class="list-inline-item">
+										<a class="btn-floating btn-sm rgba-white-slight mx-1 waves-effect waves-light">
+											<i class="fa fa-twitter bg-info"></i>
+										</a>
+									</li>
+									<li class="list-inline-item">
+										<a class="btn-floating btn-sm rgba-white-slight mx-1 waves-effect waves-light">
+											<i class="fa fa-google-plus bg-danger"></i>
+										</a>
+									</li>
+									<li class="list-inline-item">
+										<a class="btn-floating btn-sm rgba-white-slight mx-1 waves-effect waves-light">
+											<i class="fa fa-linkedin bg-linkedin"></i>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="bg-dark-purple text-white p-0">
+					<div class="container">
+						<div class="row d-flex">
+							<div class="col-lg-12 col-sm-12 mt-3 mb-3 text-center ">
+								Copyright © 2019 <a href="#" class="fs-14 text-primary">Autolist</a>. Designed by <a href="#" class="fs-14 text-primary">Spruko</a> All rights reserved.
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</section>
+		<!--Footer Section-->
+
+		<!-- Back to top -->
+		<a href="#top" id="back-to-top" ><i class="fa fa-rocket"></i></a>
+
+		<!-- JQuery js-->
+		<script src="../assets/js/jquery-3.2.1.min.js"></script>
+
+		<!-- Bootstrap js -->
+		<script src="../assets/plugins/bootstrap-4.3.1/js/popper.min.js"></script>
+		<script src="../assets/plugins/bootstrap-4.3.1/js/bootstrap.min.js"></script>
+
+		<!--JQueryVehiclerkline Js-->
+		<script src="../assets/js/jquery.sparkline.min.js"></script>
+
+		<!-- Circle Progress Js-->
+		<script src="../assets/js/circle-progress.min.js"></script>
+
+		<!-- Star Rating Js-->
+		<script src="../assets/plugins/rating/jquery.rating-stars.js"></script>
+
+		<!--Counters -->
+		<script src="../assets/plugins/counters/counterup.min.js"></script>
+		<script src="../assets/plugins/counters/waypoints.min.js"></script>
+		<script src="../assets/plugins/counters/numeric-counter.js"></script>
+
+		<!--Owl Carousel js -->
+		<script src="../assets/plugins/owl-carousel/owl.carousel.js"></script>
+
+		<!--Horizontal Menu-->
+		<script src="../assets/plugins/horizontal-menu/horizontal.js"></script>
+
+		<!--JQuery TouchSwipe js-->
+		<script src="../assets/js/jquery.touchSwipe.min.js"></script>
+
+		<!--Select2 js -->
+		<script src="../assets/plugins/select2/select2.full.min.js"></script>
+		<script src="../assets/js/select2.js"></script>
+
+		<!-- Sticky Js-->
+		<script src="../assets/js/sticky.js"></script>
+
+		<!-- Cookie js -->
+		<script src="../assets/plugins/cookie/jquery.ihavecookies.js"></script>
+		<script src="../assets/plugins/cookie/cookie.js"></script>
+
+		<!--Showmore Js-->
+		<script src="../assets/js/jquery.showmore.js"></script>
+		<script src="../assets/js/showmore.js"></script>
+
+        <!-- Custom scroll bar Js-->
+		<script src="../assets/plugins/scroll-bar/jquery.mCustomScrollbar.js"></script>
+
+		<!-- Swipe Js-->
+		<script src="../assets/js/swipe.js"></script>
+
+
+
+		<!-- Custom Js-->
+		<script src="../assets/js/custom.js"></script>
+
+		<!-- Owl Carousel Js-->
+		<script src="../assets/js/owl-carousel.js"></script>
