02425ca5084b59563ff9b9090cf35eebae3d762a
atulalive <94101683+atulalive@users.noreply.github.com>
2021-11-18 18:26:06 +0530

    car details and other index

---
+++ app/Views/prelinestyle_view.php
@@ -0,0 +1,73 @@
+<head>
+    <style>
+.horizontalMenu>.horizontalMenu-list>li>a {
+			color: #504e70 !important;
+		}
+
+		.horizontalMenu>.horizontalMenu-list>li>a.active {
+			background-color: #e72a1a !important;
+			color: #fff !important;
+		}
+
+		.horizontalMenu>.horizontalMenu-list {
+			margin: -18 px auto 0 auto !important;
+		}
+		.horizontalMenu>ul {
+			margin-top: -18px !important;
+		}
+		.modal-content {
+			border-radius: 16px !important;
+		}
+
+		.modal-header {
+			border-bottom: 0px solid #d8dde6 !important;
+		}
+
+		.modal-dialog {
+			width: 350px;
+			border-radius: 16px;
+			max-width: 360 px;
+			min-height: 382 px;
+			height: auto;
+			margin: 0 auto;
+			bottom: auto;
+			/* top: 50%; */
+			box-shadow: none;
+			transform: translateY(-50%);
+		}
+
+		button[disabled=disabled],
+		button:disabled {
+			border: 1px solid #999999 !important;
+			background-color: #24272cb3 !important;
+			color: #666666;
+			margin-right: 100%;
+		}
+
+		@media (min-width: 576px) {
+			.modal-dialog {
+				max-width: 350px;
+			}
+		}
+
+		@media (min-width: 768px) {
+			.modal-dialog {
+				max-width: 400px;
+			}
+		}
+
+		@media (min-width: 992px) {
+			.modal-dialog {
+				max-width: 550px;
+			}
+		}
+
+		@media (min-width: 1280px) {
+			.modal-dialog {
+				max-width: 750px;
+				
+			}
+		}
+             
+	</style>
+</head>
