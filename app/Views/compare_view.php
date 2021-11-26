c4a44077f66e0644f8c0caa6832cb3bfe0693976
“[firstname <adi836386@gmail.com>
2021-11-26 14:04:42 +0530

    compare car

---
+++ app/Views/compare_view.php
@@ -0,0 +1,191 @@
+
+		<!--Section-->
+		<section class="sptb bg-white">
+			<div class="container">
+				<div class="section-title center-block text-center">
+					<h2>Compare Cars</h2>
+					<p>Confused? Easy way to compare cars</p>
+				</div>
+				<div class="row">
+					<div class="card mb-0">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-xl-6 col-lg-6 col-md-12">
+									<div class="form-group search-cars1" >
+										<select class="form-control select2-show-search border-bottom-0 w-100" data-placeholder="Select">
+										<option>Select Brand/Model</option>		
+											
+												
+												<optgroup label="Maruti">
+													
+													<option value="1">Alto 800</option>
+													<option value="2">Baleno</option>
+													<option value="3">Celerio</option>
+													<option value="4">Ciaz</option>
+													<option value="5">Dzire</option>
+													<option value="6">Eeco</option>
+													<option value="7">Ertiga</option>
+													<option value="8">Lgnis</option>
+													<option value="9">S-Cross</option>
+													<option value="10">S-Presso</option>
+													<option value="11">Swift</option>
+													<option value="12">Swift Dzire Tour</option>
+													<option value="13">Vitara Breeza</option>
+													<option value="14">Wagon R</option>
+													<option value="15">XL6</option>
+													</div>
+												</optgroup>
+
+												
+												<optgroup label="Hyundal">
+													<option value="1">Alcarzar</option>
+													<option value="2">Aura</option>
+													<option value="3">Creta</option>
+													<option value="4">Elantra</option>
+													<option value="5">Grand i10 Nios</option>
+													<option value="6">Ioniq</option>
+													<option value="7">Kona Electric</option>
+													<option value="8">Santa Fe 2022</option>
+													<option value="9">Santro</option>
+													<option value="10">Tucson</option>
+													<option value="11">Venue</option>
+													<option value="12">Verna Dzire Tour</option>
+													<option value="13">Xcent</option>
+													<option value="14">i20</option>
+													<option value="15">i20 N Line</option>
+												</optgroup>
+
+												<optgroup label="Honda">
+													<option value="1">Amaze</option>
+													<option value="2">Brio 2020</option>
+													<option value="3">City</option>
+													<option value="4">City 4th Generation</option>
+													<option value="5">Jazz</option>
+													<option value="6">WR-X</option>
+											
+												</optgroup>
+
+												<optgroup label="Tata">
+													<option value="1">Altorz</option>
+													<option value="2">Harrier</option>
+													<option value="3">Nexon</option>
+													<option value="4">Nexon EY 4th Generation</option>
+													<option value="5">Punch</option>
+													<option value="6">Safari</option>
+													<option value="3">Tiago</option>
+													<option value="4">Tiagor</option>
+													<option value="5">Tigor Ex</option>
+													<option value="6">Yodha Pickup</option>
+											
+											
+												</optgroup>
+
+
+
+												<optgroup label="Mahindra">
+													<option value="1">Alturas G4</option>
+													<option value="2">Bolero</option>
+													<option value="3">Bolero Neo</option>
+													<option value="4">Bolero Pik-Up</option>
+													<option value="5">E Vertio</option>
+													<option value="6">KUV100NXT</option>
+													<option value="3">Marazzo</option>
+													<option value="4">Sorpio</option>
+													<option value="5">Thar</option>
+													<option value="6">XUV300</option>
+													<option value="6">XUV700</option>
+													<option value="6">eKUV100</option>
+											
+											
+												</optgroup>
+
+												<optgroup label="Renault">
+													<option value="1">Duster</option>
+													<option value="2">KWID</option>
+													<option value="3">Kiger</option>
+													<option value="4">Triber</option>
+												
+											
+											
+												</optgroup>
+
+
+													<optgroup label="Mini">
+
+											</optgroup>
+										</select>
+									</div>
+									<div class="form-group mb-lg-0 search-cars1">
+										<select class="form-control select2-show-search border-bottom-0 w-100" data-placeholder="Select">
+											<optgroup label="Select Variant">
+												<option>Choose Variant</option>
+												<option value="1">STD (Petrol) 3.15 Lakh*</option>
+												<option value="2">STD (Petrol) 3.21 Lakh*</option>
+												<option value="3">LXI (Petrol) 3.86 Lakh*</option>
+												<option value="4">LXI (Petrol) 3.92 Lakh*</option>
+												<option value="5">VXI (Petrol) 4.12 Lakh*</option>
+												<option value="6">VXI (Petrol) 4.26 Lakh*</option>
+												<option value="7">LXI S-CNG -(CNG) 4.76 Lakh*</option>
+												<option value="8">LXI Opt S-CNG -(CNG) 4.82 Lakh*</option>
+												
+											</optgroup>
+										</select>
+									</div>
+								</div>
+								
+									
+								<div class="col-xl-6 col-lg-6 col-md-12">
+									<a class="btn btn-white btn-block mb-4 fs-15 " href="#"><i class="fa fa-plus"></i> Compare Another Car</a>
+									<a class="btn btn-primary btn-block fs-15" href="#">Compare</a>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>	
+		</section>
+		<!--Section-->
+		<!--Section-->
+		<section class="sptb bg-white">
+			<div class="container text-center">
+				<div class="row mb-4">
+					<div class="col-md-12">
+						<h3>Renault KWID Vs Tata Punch</h3>
+					</div>
+					<div class="col-md-4 pr-md-0">
+						<div class="car-comparision car-comparision1">
+							<div class="item7-card-img d-flex">
+								<img src="../assets/images/media/f1.png" alt="img" class="cover-image w-100 op-1">
+							</div>
+							<div class="card-image-difference bg-red"><a href="#" class="text-white"><i class="fa fa-plus"></i></a></div>
+						</div>
+					</div>
+					<div class="col-md-6"style="paddning">
+						<div class="car-comparision car-comparision1">
+							<div class="item7-card-img d-flex" style="padding-top:8%;">
+								<img src="../assets/images/media/a1.jpg" alt="img" class="cover-image w-50">
+								<img src="../assets/images/media/a3.jpg" alt="img" class="cover-image w-50">
+							</div>
+							<div class="card-image-difference bg-red"><i class="fa fa-exchange"></i></div>
+							<div class="card-body p-4">
+								<div class="row">
+									<div class="col">
+										<h4>Renault KWID</h4>
+										<h6>5.66 Lakh*</h6>
+										<a class="btn btn-primary btn-block mt-3" href="car-compare.html">View Festival Offers</a>
+									</div>
+									<div class="col">
+										<h4>Tata Punch</h4>
+										<h6>9.08 Lakh*</h6>
+										<a class="btn btn-primary btn-block mt-3" href="car-compare.html">View Festival Offers</a>
+									</div>
+								</div>
+								
+							</div>
+						</div>
+					</div>
+
+</section>
+
+				
---
+++ app/Views/comparecar_pannel.php
@@ -0,0 +1,176 @@
+	<!--Car Comparison-->
+    <section class="sptb bg-patterns bg-white">
+			<div class="container">
+			<div class="parent-div bg-white" style="width: 1170px">
+			<div class="center-block text-left" style="padding-bottom:0px;">
+
+					<h2>Popular Cars Comparison</h2>
+					<hr class="deep-purple text-primary accent-2 mb-4 mt-2 d-inline-block mx-auto" style="padding-top:20px;">
+					
+				</div>
+				<div id="defaultCarousel" class="owl-carousel Card-owlcarousel owl-carousel-icons">
+					<div class="item">
+						<div class="card mb-0 car-comparision">
+							<div class="item7-card-img d-flex">
+								<img src="../assets/images/media/a1.jpg" alt="img" class="cover-image w-50">
+								<img src="../assets/images/media/a3.jpg" alt="img" class="cover-image w-50">
+							</div>
+							<div class="card-image-difference bg-red"><i class="fa fa-exchange"></i></div>
+							<div class="card-body p-4">
+								<div class="row">
+									<div class="col">	
+										<h4>KWID</h4>
+										<h6>₹ 4.11 - 5.66 Lakh*</h6>
+									</div>
+									<div class="col">
+										<h4>Punch</h4>
+										<h6>₹ 5.48 - 9.08 Lakh*</h6>
+									</div>
+								</div>
+								<a class="btn btn-primary btn-block mt-3" href="car-compare.html">View Offers</a>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="card mb-0 car-comparision">
+							<div class="item7-card-img d-flex">
+								<img src="../assets/images/media/a4.jpg" alt="img" class="cover-image w-50">
+								<img src="../assets/images/media/a5.jpg" alt="img" class="cover-image w-50">
+							</div>
+							<div class="card-image-difference bg-red"><i class="fa fa-exchange"></i></div>
+							<div class="card-body p-4">
+								<div class="row">
+									<div class="col">
+										<h4>KWID</h4>
+										<h6>₹ 4.11 - 5.66 Lakh*</h6>
+									</div>
+									<div class="col">
+										<h4>Celerio 2017-2021</h4>
+										<h6>₹ 6.12 - 5.00 Lakh*</h6>
+									</div>
+								</div>
+								<a class="btn btn-primary btn-block mt-3" href="car-compare.html">View Offers</a>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="card mb-0 car-comparision ">
+							<div class="item7-card-img d-flex bg-red">
+								<img src="../assets/images/media/a6.jpg" alt="img" class="cover-image w-50">
+								<img src="../assets/images/media/a7.jpg" alt="img" class="cover-image w-50">
+							</div>
+							<div class="card-image-difference bg-red"><i class="fa fa-exchange"></i></div>
+							<div class="card-body p-4">
+								<div class="row">
+									<div class="col">
+										<h4>Creta</h4>
+										<h6>₹ 10.16 - 17.87 Lakh*</h6>
+									</div>
+									<div class="col">
+										<h4>Seltos</h4>
+										<h6>₹ 9.95 - 18.10 Lakh *</h6>
+									</div>
+								</div>
+								<a class="btn btn-primary btn-block mt-3" href="car-compare.html">View Offers</a>					</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="card mb-0 car-comparision">
+							<div class="item7-card-img d-flex">
+								<img src="../assets/images/media/a8.jpg" alt="img" class="cover-image w-50">
+								<img src="../assets/images/media/a9.jpg" alt="img" class="cover-image w-50">
+							</div>
+							<div class="card-image-difference bg-red"><i class="fa fa-exchange"></i></div>
+							<div class="card-body p-4">
+								<div class="row">
+									<div class="col">
+										<h4>Altroz</h4>
+										<h6>₹ 5.89 - 9.64 Lakh*</h6>
+									</div>
+									<div class="col">
+										<h4>Punch</h4>
+										<h6>₹ 5.48 - 9.08 Lakh*</h6>
+									</div>
+								</div>
+								<a class="btn btn-primary btn-block mt-3" href="car-compare.html">View Offers</a>
+
+							
+
+							</div>
+						</div>
+					</div>
+
+					<div class="item">
+						<div class="card mb-0 car-comparision">
+							<div class="item7-card-img d-flex">
+								<img src="../assets/images/media/a10.jpg" alt="img" class="cover-image w-50">
+								<img src="../assets/images/media/a9.jpg" alt="img" class="cover-image w-50">
+							</div>
+							<div class="card-image-difference bg-red"><i class="fa fa-exchange"></i></div>
+							<div class="card-body p-4">
+								<div class="row">
+									<div class="col">
+										<h4>Magnite</h4>
+										<h6>₹ 5.71 - 10.15 Lakh *</h6>
+									</div>
+									<div class="col">
+										<h4>Punch</h4>
+										<h6>₹ 5.48 - 9.08 Lakh *</h6>
+									</div>
+								</div>
+								<a class="btn btn-primary btn-block mt-3" href="car-compare.html">View Offers</a>
+
+							</div>
+						</div>
+					</div>
+
+					<div class="item">
+						<div class="card mb-0 car-comparision">
+							<div class="item7-card-img d-flex">
+								<img src="../assets/images/media/a11.jpg" alt="img" class="cover-image w-50">
+								<img src="../assets/images/media/a12.jpg" alt="img" class="cover-image w-50">
+							</div>
+							<div class="card-image-difference bg-red"><i class="fa fa-exchange"></i></div>
+							<div class="card-body p-4">
+								<div class="row">
+									<div class="col">
+										<h4>Ertiga</h4>
+										<h6>₹ 7.96 - 10.69 Lakh *</h6>
+									</div>
+									<div class="col">
+										<h4>XL6</h4>
+										<h6>₹ 9.98 - 11.86 Lakh *</h6>
+									</div>
+								</div>
+								<a class="btn btn-primary btn-block mt-3" href="car-compare.html">View Offers</a>
+
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="card mb-0 car-comparision">
+							<div class="item7-card-img d-flex">
+								<img src="../assets/images/media/a13.jpg" alt="img" class="cover-image w-50">
+								<img src="../assets/images/media/a14.jpg" alt="img" class="cover-image w-50">
+							</div>
+							<div class="card-image-difference bg-red"><i class="fa fa-exchange"></i></div>
+							<div class="card-body p-4">
+								<div class="row">
+									<div class="col">
+										<h4>Taigun</h4>
+										<h6>₹ 10.54 - 17.54 Lakh *</h6>
+									</div>
+									<div class="col">
+										<h4>Kushaq</h4>
+										<h6>₹ 10.79 - 17.99 Lakh *</h6>
+									</div>
+								</div>
+								<a class="btn btn-primary btn-block mt-3" href="car-compare.html">View Offers</a>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--Car Comparison-->
